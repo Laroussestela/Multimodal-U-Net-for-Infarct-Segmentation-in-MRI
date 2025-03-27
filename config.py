32022a5452ae508d047a597fbfdbfaea02be8b4a
Laroussestela <62725709+Laroussestela@users.noreply.github.com>
2025-03-27 17:27:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
 import os
 
-# Configuración de parámetros
 VOLUME_START_AT = 20
 VOLUME_SLICES = 32
 IMG_SIZE = 112
-IMG_HEIGHT = IMG_SIZE  # Asumimos que la imagen es cuadrada
-IMG_WIDTH = IMG_SIZE  # Asumimos que la imagen es cuadrada
-IMG_CHANNELS = 2  # Dos canales (ADC y DWI)
-n_classes = 2  # Número de clases para la segmentación
+IMG_HEIGHT = IMG_SIZE
+IMG_WIDTH = IMG_SIZE
+IMG_CHANNELS = 2
+n_classes = 2
 
-# Directorios de datos
 RAW_DATA_PATH = "/content/drive/MyDrive/Colab Notebooks/Multimodal_Unet_Infart_Segmentation"
 DERIVATIVES_PATH = "/content/drive/MyDrive/Colab Notebooks/Multimodal_Unet_Infart_Segmentation"
 
-# Ruta para guardar modelos
-MODEL_SAVE_PATH = "/content/drive/MyDrive/Colab Notebooks/Multimodal_Unet_Infart_Segmentation"
+MODEL_SAVE_PATH = "/content/drive/MyDrive/Colab Notebooks/Multimodal_Unet_Infart_Segmentation/model"
